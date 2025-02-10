11dd0d5b1f1309d71edf6fb6c92ba20fafe6a2e2
Vladislav Kolegov <dev@vkolegov.ru>
2025-02-11 01:53:11 +0700

    removed old api

--- src/api.js
+++ src/api.js
@@ -1,11 +1,15 @@
 import {
-    baseURL_GO,
+    baseURL,
     getRequest,
     postRequest,
     putRequest,
 } from '@/basic_api.js';
 
-// new api (go)
+/**
+ * @typedef {Object} LoginResponse
+ * @property {string} token
+ * @property {Object} user
+ */
 
 /**
  *
@@ -13,7 +17,7 @@ import {
  * @returns {Promise<LoginResponse>}
  */
 export async function authUsingTelegram(initData) {
-    return await postRequest(`${baseURL_GO}/telegram/auth`, {
+    return await postRequest(`${baseURL}/telegram/auth`, {
         tg_raw_data: initData,
     });
 }
@@ -25,7 +29,7 @@ export async function authUsingTelegram(initData) {
  * @returns {Promise<LoginResponse>}
  */
 export async function login(phone, password) {
-    return await postRequest(`${baseURL_GO}/users/login`, {
+    return await postRequest(`${baseURL}/users/login`, {
         phone,
         password,
     });
@@ -33,11 +37,11 @@ export async function login(phone, password) {
 
 
 export async function fetchMe() {
-    return await getRequest(`${baseURL_GO}/users/me`);
+    return await getRequest(`${baseURL}/users/me`);
 }
 
 export async function fetchUserCars() {
-    return await getRequest(`${baseURL_GO}/cars`);
+    return await getRequest(`${baseURL}/cars`);
 }
 
 /**
@@ -45,7 +49,7 @@ export async function fetchUserCars() {
  * @returns {Promise<HistoryRecord[]>}
  */
 export async function fetchHistoryRecords(carId = null) {
-    const url = `${baseURL_GO}/history_records`;
+    const url = `${baseURL}/history_records`;
 
     if (carId) {
         return await getRequest(url, {
@@ -63,7 +67,7 @@ export async function fetchHistoryRecords(carId = null) {
  * @returns {Promise<HistoryRecord>}
  */
 export async function fetchHistoryRecord(id) {
-    return await getRequest(`${baseURL_GO}/history_records/${id}`);
+    return await getRequest(`${baseURL}/history_records/${id}`);
 }
 
 
@@ -72,12 +76,12 @@ export async function fetchHistoryRecord(id) {
  * @returns {Promise<HistoryRecord>}
  */
 export async function createHistoryRecord(data) {
-    return await postRequest(`${baseURL_GO}/history_records`, data);
+    return await postRequest(`${baseURL}/history_records`, data);
 }
 
 
 export async function updateHistoryRecord(recordId, data) {
-    return await putRequest(`${baseURL_GO}/history_records/${recordId}`, data);
+    return await putRequest(`${baseURL}/history_records/${recordId}`, data);
 }
 
 
@@ -87,7 +91,7 @@ export async function updateHistoryRecord(recordId, data) {
  * @returns {Promise<TotalCarStats>}
  */
 export async function fetchCarStats(carId) {
-    return await getRequest(`${baseURL_GO}/stats/car/${carId}`);
+    return await getRequest(`${baseURL}/stats/car/${carId}`);
 }
 
 
@@ -98,7 +102,7 @@ export async function fetchCarStats(carId) {
  * @returns {Promise<HistoryRecord>}
  */
 export async function fetchBeforeDate(carId, date) {
-    return await getRequest(`${baseURL_GO}/history_records/before_date`, {
+    return await getRequest(`${baseURL}/history_records/before_date`, {
         car_id: carId,
         date: date.toISOString(),
     })
@@ -110,6 +114,6 @@ export async function fetchBeforeDate(carId, date) {
  * @returns {Promise<{response: string, cost: number}>}
  */
 export async function getAiItemsDescription(items) {
-    return await postRequest(`${baseURL_GO}/ai/invoice_items_description`,
+    return await postRequest(`${baseURL}/ai/invoice_items_description`,
         { items });
 }
