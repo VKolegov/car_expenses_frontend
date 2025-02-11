afe04e8094386312e5c53543779c6000283f1eaa
Vladislav Kolegov <dev@vkolegov.ru>
2025-02-11 20:01:14 +0700

    main page: conditional redirect

--- src/pages/TheWelcome.vue
+++ src/pages/TheWelcome.vue
@@ -1,7 +1,18 @@
 <script setup>
 import { useUserStore } from '@/store.js';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 
-const { user } = useUserStore();
+const router = useRouter();
+const { user, userCars } = useUserStore();
+
+onMounted(() => {
+  if (user && userCars.length > 0) {
+    router.push({name: 'history'});
+  } else {
+    router.push({name: 'garage'});
+  }
+})
 </script>
 
 <template>
