e7b09e5dd8ca56773f2d6f57f289f34b2116928f
Vladislav Kolegov <dev@vkolegov.ru>
2025-02-11 19:55:20 +0700

    car selector: logo

--- src/components/CarSelector.vue
+++ src/components/CarSelector.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { computed, onMounted, ref } from 'vue';
-import { VSelect } from 'vuetify/components';
+import { VSelect, VListItem } from 'vuetify/components';
 
 import { useUserStore } from '@/store.js';
 
@@ -24,9 +24,33 @@ onMounted(async () => {
       label="Автомобиль"
       :items="cars"
       :item-title="car => `${car.brand} ${car.model} ${car.generation_info.short_name}`"
-  ></v-select>
+  >
+    <template v-slot:item="{ props, item }">
+      <v-list-item v-bind="props">
+        <template v-slot:prepend>
+          <img
+              :src="item.raw.brand_info.logo_image"
+              class="car-list-item__brand-logo"
+          >
+        </template>
+      </v-list-item>
+    </template>
+    <template v-slot:selection="{ item, index }">
+      <div style="display: flex; align-items: center;">
+        <img
+            :src="item.raw.brand_info.logo_image"
+            class="car-list-item__brand-logo"
+        >
+        <span>{{ `${item.raw.model} ${item.raw.generation_info.short_name}` }}</span>
+      </div>
+    </template>
+  </v-select>
 </template>
 
 <style scoped>
-
+.car-list-item__brand-logo {
+  margin-right: 5px;
+  width: 16px;
+  height: 16px;
+}
 </style>
